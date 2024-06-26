b7d5ef04ca510ace91dcb2a28da8f77d05173396
kimdust <mji1223@naver.com>
2024-06-26 11:20:31 +0900

    next.js start

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>KRDS - next.js 템플릿</title>
+      </Head>
+      <main>
+        <h1>Welcome to KRDS - next.js 템플릿</h1>
+      </main>
+    </>
+  );
+}
